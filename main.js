580657e5c32d2d2a347fdb6ec67f650c3b8c88b3
ETLopes <eduardotorreslopes@gmail.com>
2022-12-08 23:22:16 -0300

    refactor: refactor methods

--- main.js
+++ main.js
@@ -12,27 +12,31 @@ const client = new vision.ImageAnnotatorClient({
 
 // Performs label detection on the image file
 
-function sortByXY(arr) {
-  arr.sort((a, b) => {
-    const x1 = a["boundingPoly"]["vertices"][0]["x"];
-    const y1 = a["boundingPoly"]["vertices"][0]["y"];
-    const x2 = b["boundingPoly"]["vertices"][0]["x"];
-    const y2 = b["boundingPoly"]["vertices"][0]["y"];
+function sortByXY(array) {
+  array.sort((a, b) => {
+    const verticesA = a["boundingPoly"]["vertices"][0];
+    const verticesB = b["boundingPoly"]["vertices"][0];
+    const x1 = verticesA["x"];
+    const y1 = verticesA["y"];
+    const x2 = verticesB["x"];
+    const y2 = verticesB["y"];
+
     if (x1 === x2) {
       return y1 - y2;
     } else {
       return x1 - x2;
     }
   });
-  return arr;
+
+  return array;
 }
 
-function getDescriptionsByY(arr) {
+function getDescriptionsByY(array) {
   const descriptionsByY = new Map();
 
-  for (const obj of arr) {
-    const y = obj["boundingPoly"]["vertices"][0]["y"];
-
+  for (const obj of array) {
+    const vertices = obj["boundingPoly"]["vertices"][0];
+    const y = vertices["y"];
     const description = obj["description"];
 
     if (descriptionsByY.has(y)) {
