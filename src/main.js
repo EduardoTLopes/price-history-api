85281598e85c11fc680e127410f91d6aab5d13c0
Emiliano Leite <leiteemiliano@gmail.com>
2023-05-07 11:00:32 -0300

    revert unnecessary change

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ const { startupBot } = require("./telegram/index");
 async function startup() {
   startupBot();
 
-  app.get("/", (req, res) => {
-    return res.send("Hello world")
-  });
+  app.get("/", (req, res) => res.send("Hello world"));
 
   app.listen(5000, "127.0.0.1", () =>
     console.log("Server running on port 5000")
