2aae5a78122d24a8ca92153ab7a6ba172c0da325
Emiliano Leite <emiliano.leite@klarna.com>
2023-05-11 13:09:49 +0200

    Convert openai file to TS

---
+++ src/openai/index.ts
@@ -0,0 +1,17 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY ?? '',
+});
+const openai = new OpenAIApi(configuration);
+
+export async function readReceipt(data: unknown) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `based on this JSON: ${JSON.stringify(data)} return the total value of the receipt in the following format: R$ 10,00. The response should contain only the currency and the value.`,
+    temperature: 0,
+    max_tokens: 10,
+  })
+
+  return completion
+}
